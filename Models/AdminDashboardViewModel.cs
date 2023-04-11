1398ddf435daa008fae5003ab41e99e8ba417ac7
Sachin Sharma <106727280+Sachin-Sharma-0@users.noreply.github.com>
2023-04-11 17:20:21 +0530

    Create AdminDashboardViewModel.cs

---
+++ Models/AdminDashboardViewModel.cs
@@ -0,0 +1,11 @@
+namespace HotelMgmtSys.Models
+{
+    public class AdminDashboardViewModel
+    {
+        public int AvailableRoomsCount { get; set; }
+        public int OccupiedRoomsCount { get; set; }
+        public int RegisteredUsersCount { get; set; }
+        public int EntryLogsCount { get; set; }
+        public int ExitLogsCount { get; set; }
+    }
+}
