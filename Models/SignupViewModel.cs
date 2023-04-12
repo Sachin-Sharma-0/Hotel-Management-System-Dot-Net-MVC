15583f6f4e80c6d71304f6b60b1003f276043771
Sachin Sharma <106727280+Sachin-Sharma-0@users.noreply.github.com>
2023-04-12 17:42:54 +0530

    Create SignupViewModel.cs

---
+++ Models/SignupViewModel.cs
@@ -0,0 +1,18 @@
+namespace HotelMgmtSys.Models
+{
+    public class SignupViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public double MobileNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string IdProof { get; set; }
+        public string Gender { get; set; }
+
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
